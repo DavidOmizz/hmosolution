4babf0ec8272c3c20593ec12ba4e1cdc63103667
David Omisakin <davidomisakin4good@gmail.com>
2024-05-29 12:11:20 +0100

    Fixed forget password logic

--- hmosolution/settings.py
+++ hmosolution/settings.py
@@ -181,3 +181,16 @@ SOCIALACCOUNT_PROVIDERS = {
         # 'GRAPH_API_URL': 'https://graph.facebook.com/v13.0',
     }
 }
+
+
+
+# email configs
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+# EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
+EMAIL_HOST_USER = 'davidomisakin4good@gmail.com'
+# EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
+EMAIL_HOST_PASSWORD = 'mnkc nonx esri unjm' #for hmosolution
